0b8cf413d1897ba9b5672d3fa19797e80bf89a88
Johni <https://github.com/johnicassere>
2024-07-14 04:46:17 -0300

    finish

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,74 @@
 <script setup>
 import { reactive } from 'vue';
 
-
 const estado = reactive({
-  valor: 0,
-  operacoes: ["soma", "subtracao", "multiplicacao", "divisao"]
+    operacao: "",
+    valor: 0,
+    valorIput1: '',
+    valorIput2: '',
+    textoInicial:"Você Escolheu " 
+     
 });
 
-function escolhaDaOperacao(evento){
-  estado.valor = evento.target.value
-  
+function opEscolhida(oper) {
+    estado.valorIput1 = '';
+    estado.valorIput2 = '';
+    if(estado.operacao == ''){
+    alert("Escolha a Operação")
+    estado.valorIput1 = '';
+    estado.valorIput2 = '';
+  }  
+    return estado.operacao = oper.target.value
+ }
+
+function valorDigitado1(n1){
+  let valor = parseInt(n1.target.value)
+  if(estado.operacao == ''){
+    alert("Escolha a Operação")
+    estado.valorIput1 = '';
+    estado.valorIput2 = '';
+  }
+  return valor;
+
 }
 
-function soma(){
-  estado.valor + estado.valor
+function valorDigitado2(n2){
+  let valor = parseInt(n2.target.value)
+  if(estado.operacao == ''){
+    alert("Escolha a Operação")
+    estado.valorIput1 = '';
+    estado.valorIput2 = '';
+  }
+  
+ return valor, valorOperado();
 }
-function subtracao(){
-  valor - valor
+
+function valorOperado(operacao, n1, n2 ){
+  operacao = estado.operacao;
+  n1 = estado.valorIput1
+  n2 = estado.valorIput2
+
+  estado.valorIput1 = ''
+  estado.valorIput2 = ''
+
+  switch(operacao){
+      case "Soma" :
+      return estado.valor = n1 + n2
+    
+      case "Subtracao" :
+      return estado.valor = (n1 - n2)
+      
+      case "Multiplicacao" :
+      return estado.valor = n1 * n2
+      
+      case "Divisao" :
+      return estado.valor = n1 / n2
+
+      default:
+        estado.valor
+  }
 }
-function multiplicacao(){
-  valor * valor
-}
-function divisao(){
-  valor / valor
-}
+
 
 </script>
 
@@ -31,31 +76,38 @@ function divisao(){
   <header>
     <div class="logo">
       <img alt="Vue logo" src="./assets/logo.svg" width="50" height="50" />
-      <h1>Exercicio vue modulo 27</h1>
+      <h1>Exercício vue modulo 27</h1>
     </div>
   </header>
 
   <body>
     <div class="container">
       <section class="section__resultado">
-            <h3>Resultado da Operação</h3>
+            <h3 v-if="estado.operacao != ''">{{ estado.textoInicial + estado.operacao }}</h3>
+            <h3 v-if="estado.valor > 0">Resultado da Operação é:</h3>
             <span>{{ estado.valor }}</span>
       </section>
 
       <section class="section__operacao">
                 <h3>Escolha a Operção</h3>
-                <select>
-                    <option v-for="operacao in estado.operacoes">{{ operacao }}</option>
-                </select>
+                    <select @change="opEscolhida($event)" v-model="estado.operacao">
+                     <option value="Soma">Soma</option>
+                     <option value="Subtracao">Subtracão</option>
+                     <option value="Multiplicacao">Multiplicacão</option>
+                     <option value="Divisao">Divisão</option>
+                  </select>
         </section>
-    
+
           <section class="section__valor">
-            <label for="">Digite o Valor</label>
+            <label>Digite o Primeiro Valor</label>
             <div>
-              <input @keyup="escolhaDaOperacao" type="number">
+              <input @keyup="valorDigitado1" v-model="estado.valorIput1" type="number">
+            </div>
+            <label>Digite o Segundo Valor</label>
+            <div>
+              <input @keyup="valorDigitado2" v-model="estado.valorIput2" type="number">
             </div>
           </section>
-
     </div>
     
   </body>
@@ -90,14 +142,12 @@ function divisao(){
 
 .section__valor{
   color: rgb(135, 23, 240);
-  padding: 16px;
-  display: flex;
-  flex-direction: column;
 }
+
 
 .section__valor input{
   padding: 4px;
-  margin-top: 16px;
+  margin-bottom: 16px;
   text-align: center;
   width: 200px;
   border-radius: 4px;
